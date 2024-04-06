4bb2965c3823224344cc16d1f1992e5c9fe30b64
Julio Macias <159674162+jamzlibra@users.noreply.github.com>
2024-04-06 07:45:33 -0400

    Generar numero del 0 al 99 + 1

---
+++ src/AdivinaElNumero.java
@@ -0,0 +1,49 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class AdivinaElNumero {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        Random random = new Random();
+
+        boolean jugarDeNuevo = true;
+
+        while (jugarDeNuevo) {
+            int numeroAdivinar = random.nextInt(100) + 1; // Generar número aleatorio entre 0 a 99 + 1
+            int intentos = 5;
+
+            System.out.println("Bienvenido al juego de adivinanzas. Debes adivinar un número entre 1 y 100.");
+
+            while (intentos > 0) {
+                System.out.println("Tienes " + intentos + " intentos restantes.");
+                System.out.println("Ingresa tu número:");
+                int numeroUsuario = teclado.nextInt();
+
+                if (numeroUsuario == numeroAdivinar) {
+                    System.out.println("¡Felicidades! ¡Has adivinado el número!");
+                    break;
+                } else if (numeroUsuario < numeroAdivinar) {
+                    System.out.println("El número a adivinar es mayor.");
+                } else {
+                    System.out.println("El número a adivinar es menor.");
+                }
+
+                intentos--;
+            }
+
+            if (intentos == 0) {
+                System.out.println("¡Lo siento! Has agotado tus 5 intentos. El número a adivinar era: " + numeroAdivinar);
+            }
+
+            System.out.println("¿Desea jugar de nuevo? (Sí: 's' o 'S', No: cualquier otra tecla)");
+            char opcion = teclado.next().charAt(0);
+
+            if (opcion != 's' && opcion != 'S') {
+                jugarDeNuevo = false;
+            }
+        }
+
+        System.out.println("¡Gracias por jugar!");
+        teclado.close();
+    }
+}
